71ab75a24d51d8807b51576ba108012cfc4ed2e5
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-09-20 14:48:58 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Waiter
 
+    attr_reader :name, :years_of_experience
+
+    @@all = []
+
+    def initialize(name, years_of_experience)
+        @name = name
+        @years_of_experience = years_of_experience
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+
+    def new_meal(customer, total, tip)
+        Meal.new(waiter = self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        tippers = self.meals.sort_by do |meal|
+            meal.tip
+        end
+        tippers[-1].customer
+    end
 end
